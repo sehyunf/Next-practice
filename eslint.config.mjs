4419a2cd4ac33acf8da288728234a15edddfbda4
sehyunf <ssam010925@gmail.com>
2025-05-13 14:19:20 +0900

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends : ["next/core-web-vitals", "next/typescript", "prettier"], // 충돌방지 "prettier" 추가
+    rules : {
+      // semi : ["error"], // ex) 세미콜론 없을 시 에러
+      // quotes : ["error", "double"], // ex) 쿼터는 무조건 대문자,
+      // "prefer-arrow-callback" : ["error"], // ex) 콜백함수는 무조건 에로우 함수
+    }
+  }),
 ];
 
 export default eslintConfig;
