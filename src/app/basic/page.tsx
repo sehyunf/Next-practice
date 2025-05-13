ebb13c59ee99fc58195f718629ecf3db14a5ac16
sehyunf <ssam010925@gmail.com>
2025-05-13 14:34:19 +0900

    next app routing

---
+++ src/app/basic/page.tsx
@@ -0,0 +1,12 @@
+// import { useSearchParams } from "next/navigation";
+
+export default function basicpage() {
+
+  // const {param} = useSearchParams();
+
+  return (
+    <div>
+      <p>hello app routing</p>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
+import "./styles/globals.css"
 
 export const metadata: Metadata = {
   title: "Next project",
   description: "재밌게 공부하는 Next.js",
+  icons : {
+    icon : "/assets/images/logo/favicon.ico"
+  }
 };
 
 export default function RootLayout({
